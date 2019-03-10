a78e2ffd776698e07f62489216a5c8fc54fa8a4e
reiniergs <reiniergs@gmail.com>
2019-03-09 23:35:13 -0500

    feat: list table data

---
+++ src/modules/my/app/fetchData.js
@@ -0,0 +1,28 @@
+const recordMetadata = {
+    name: 'name',
+    email: 'email',
+    website: 'url',
+    amount: 'currency',
+    phone: 'phoneNumber',
+    closeAt: 'dateInFuture',
+};
+
+const currencies = ['USD', 'EUR', 'MXN'];
+
+export default function fetchDataHelper({ amountOfRecords }) {
+    return fetch('https://data-faker.herokuapp.com/collection', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8',
+        },
+        body: JSON.stringify({
+            amountOfRecords,
+            recordMetadata,
+        }),
+    }).then(response => response.json())
+        .then(collection => collection.map(row => ({
+            ...row,
+            currencyCode: 'EUR',
+            currencies,
+        })));
+}
