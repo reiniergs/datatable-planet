e5199eaf7f2e7e02f4593ffae6889c5fe9ff2f14
Reinier Guerra <reinier.guerra@salesforce.com>
2019-03-15 17:18:12 -0400

    chore: enable jest tests

---
+++ src/modules/my/datatable/__tests__/datatable.spec.js
@@ -0,0 +1,8 @@
+import Datatable from 'my/datatable';
+import { createElement } from 'lwc';
+
+describe('my-datatable component', () => {
+    it('should', () => {
+        expect(true).toBe(true);
+    });    
+});
