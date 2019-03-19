1a69dd85d6ee41502f2c4c294c99a0cad44264ef
reiniergs <reiniergs@gmail.com>
2019-03-19 17:43:37 -0400

    feat: set columns width initially

--- src/modules/my/datatable/datatable.js
+++ src/modules/my/datatable/datatable.js
@@ -2,7 +2,7 @@ import { LightningElement, api } from 'lwc';
 import mix from 'my/mixinBuilder';
 import eventEmitter, { emit } from './mixins/eventEmitter/index';
 import table from './mixins/table/index';
-import { normalizeColumns } from './columns'
+import { normalizeColumns } from './columns';
 
 function normalizeData(value) {
     if (Array.isArray(value)) {
@@ -13,12 +13,17 @@ function normalizeData(value) {
 
 const privateData = Symbol('privateData');
 const privateColumns = Symbol('privateColumns');
+const privateHasDefinedColumns = Symbol('privateHasDefinedColumns');
 
 export default class Datatable extends mix(LightningElement)
     .with(eventEmitter, table) {
 
         [privateData] = [];
         [privateColumns] = [];
+        [privateHasDefinedColumns] = false;
+
+        @api minColumnWidth = 50;
+        @api maxColumnWidth = 1000;
 
         @api set data(value) {
             const data = normalizeData(value);
@@ -40,5 +45,20 @@ export default class Datatable extends mix(LightningElement)
 
         get columns() {
             return this[privateColumns];
+        }
+
+        get tableStyles() {
+            const { tableWidth } = this;
+            return tableWidth !== 0 ? `width: ${tableWidth}px` : undefined;
+        }
+
+        renderedCallback() {
+            const columns = this[privateColumns];
+            const hasDefinedColumns = this[privateHasDefinedColumns];
+            if (columns.length > 0 && !hasDefinedColumns) {
+                const data = this[privateData];
+                this[emit]('SET_COLUMNS_WIDTH', { columns, data });
+                this[privateHasDefinedColumns] = true;
+            }
         }
 }
